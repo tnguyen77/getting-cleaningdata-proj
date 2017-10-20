316c12aef4d60be717b8d15ae7f3ec5222ff1a1b
Thu Nguyen <t.nguyen2@shell.com>
2017-10-20 15:04:46 -0500

    Delete run_analyst.R

--- run_analyst.R
+++
@@ -1,8 +0,0 @@
-
-features     = read.table('./features.txt',header=FALSE); 
-activityType = read.table('./activity_labels.txt',header=FALSE); 
-subjectTrain = read.table('./train/subject_train.txt',header=FALSE);
-xTrain       = read.table('./train/x_train.txt',header=FALSE); 
-yTrain       = read.table('./train/y_train.txt',header=FALSE);
-
-
