e7e1fda2197ecd5898942f6d48a7ee566816cefa
Thu Nguyen <t.nguyen2@shell.com>
2017-10-20 15:04:25 -0500

    Add files via upload

---
+++ run_analyst.R
@@ -0,0 +1,8 @@
+
+features     = read.table('./features.txt',header=FALSE); 
+activityType = read.table('./activity_labels.txt',header=FALSE); 
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE);
+xTrain       = read.table('./train/x_train.txt',header=FALSE); 
+yTrain       = read.table('./train/y_train.txt',header=FALSE);
+
+
